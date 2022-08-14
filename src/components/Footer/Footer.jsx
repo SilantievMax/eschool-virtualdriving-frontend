0d61be22de567ad914c016b2792ada879efbb35f
SilantievMax <nohrin.max@yandex.ru>
2022-08-15 02:55:55 +0300

    the AboutText component has been modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import './footer.css'
+import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
     return (
         <>
         <footer className="footer">
             <div className="footer__content">
-                <a href="#" className="footer__politik-link">Политика конфиденциальности</a>
+                <NavLink to={'/'} className="footer__politik-link">Политика конфиденциальности</NavLink>
                 <span className="footer__copyright">Сайт находится в разработке. Powered by VDES © 2020</span>
             </div> 
         </footer>
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import AboutImages from '../../components/AboutImages/AboutImages';
+import AboutText from '../../components/AboutText/AboutText';
+import BackgroundForContents from '../../components/BackgroundForContents/BackgroundForContents'
+import Footer from '../../components/Footer/Footer'
 
 const AboutPage = () => {
     return (
         <>
         <AboutImages />
+        <BackgroundForContents>
+            <AboutText />
+            <Footer />
+        </BackgroundForContents>
         </>
     )
 }
