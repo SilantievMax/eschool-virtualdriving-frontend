729f3db6f2a0a841c09261ea956ca1b2bf448d95
SilantievMax <nohrin.max@yandex.ru>
2022-08-15 19:28:42 +0300

    intermediate commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import card3 from '../../static/images/card3.webp'
 
-const LoginPage = () => {
+const Login = () => {
     return (
         <>
-        LoginPage
+        <section className="login">
+            <img src={card3} alt="card3" className="login__images" />
+            <form action="" className="login__form">
+                <h3 className="login__heading">Авторизация</h3>
+                <input type="text" className="login__input" placeholder='E-mail*' />
+                <input type="password" className="login__input" placeholder='Пароль' />
+                <a href="/" className="login__forgot">Забыли пароль?</a>
+                <div className="login__buttons">
+                    <button className="login__btn">Войти</button>
+                    <button className="login__btn-discord"></button>
+                </div>
+                <a href="/" className="login__register">Регистрация</a>
+            </form>
+        </section>
         </>
     )
 }
 
-export default LoginPage;
+export default Login;
--- src/pages/AuthorizationPage/AuthorizationPage.jsx
+++ src/pages/AuthorizationPage/AuthorizationPage.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Login from "../../components/Login/Login";
+import Register from "../../components/Register/Register";
 
 const AuthorizationPage = () => {
     return (
-        <>AuthorizationPage</>
+        <>
+        <Login />
+        <Register />
+        </>
     )
 }
 
