fb618482a8e8e97b6686e9d3004373b55a7df9f6
SilantievMax <nohrin.max@yandex.ru>
2022-08-27 12:56:21 +0300

    modified the component BahherSimulators

---
+++ src/components/BannerSimulators/BannerSimulators.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import simulators1 from '../../static/images/simulators1.webp'
+import simulators2 from '../../static/images/simulators2.webp'
+import simulators3 from '../../static/images/simulators3.webp'
+import styles from './BannerSimulators.module.css'
+
+const BannerSimulators = () => {
+    return (
+        <>
+        <section className={styles.section}>
+            <ul className={styles.list}>
+                <li className={styles.item}>
+                    <img src={simulators1} alt="" className={styles.img} />
+                </li>
+                <li className={styles.item}>
+                    <img src={simulators2} alt="" className={styles.img} />
+                </li>
+                <li className={styles.item}>
+                    <img src={simulators3} alt="" className={styles.img} />
+                </li>
+            </ul>
+        </section>
+        </>
+    )
+}
+
+export default BannerSimulators;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -9,51 +9,33 @@ import card3 from '../../static/images/card3.webp'
 const Services = () => {
     return (
         <>
-        <section class={styles.services}>
-            <h3 class={styles.heading}>Услуги</h3>
-            <ul class={styles.list}>
+        <section className={styles.services}>
+            <h3 className={styles.heading}>Услуги</h3>
+            <ul className={styles.list}>
                 <Link to={'/custom/lessons'} title="Подробнее">
-                    <li class={styles.item}>
-                        <img src={card1} alt="img" class={styles.images_card} />
-                        <h4 class={styles.heading_card}>Персональные занятия</h4>
-                        <p class={styles.description_card}>Онлайн тренировка, которая поможет сделать Вас быстрее на любой трассе</p>
-                        <div class={styles.price_card}>1500₽</div>
+                    <li className={styles.item}>
+                        <img src={card1} alt="img" className={styles.images_card} />
+                        <h4 className={styles.heading_card}>Персональные занятия</h4>
+                        <p className={styles.description_card}>Онлайн тренировка, которая поможет сделать Вас быстрее на любой трассе</p>
+                        <div className={styles.price_card}>1500₽</div>
                         <OrderButton link='/'/>
                     </li>
                 </Link>
                 <Link to={'/custom/files'} title="Подробнее">
-                    <li class={styles.item}>
-                        <img src={card2} alt="img" class={styles.images_card} />
-                        <h4 class={styles.heading_card}>Сетапы</h4>
-                        <p class={styles.description_card}>Поделимся нашими сетапами или реплеями с телеметрией быстрых кругов</p>
-                        <div class={styles.price_card}>500₽</div>
+                    <li className={styles.item}>
+                        <img src={card2} alt="img" className={styles.images_card} />
+                        <h4 className={styles.heading_card}>Сетапы</h4>
+                        <p className={styles.description_card}>Поделимся нашими сетапами или реплеями с телеметрией быстрых кругов</p>
+                        <div className={styles.price_card}>500₽</div>
                         <OrderButton link='/'/>
                     </li>
                 </Link>
                 <Link to={'/custom/liveries'} title="Подробнее">
-                    <li class={styles.item}>
-                        <img src={card3} alt="img" class={styles.images_card} />
-                        <h4 class={styles.heading_card}>Ливреи</h4>
-                        <p class={styles.description_card}>Создадим с нуля или воспроизведём по примерам дизайн гоночного автомобиля</p>
-                        <div class={styles.price_card}>2000₽</div>
-                        <OrderButton link='/'/>
-                    </li>
-                </Link>
-                <Link to={'/custom/files'} title="Подробнее">
-                    <li class={styles.item}>
-                        <img src={card2} alt="img" class={styles.images_card} />
-                        <h4 class={styles.heading_card}>Сетапы</h4>
-                        <p class={styles.description_card}>Поделимся нашими сетапами или реплеями с телеметрией быстрых кругов</p>
-                        <div class={styles.price_card}>500₽</div>
-                        <OrderButton link='/'/>
-                    </li>
-                </Link>
-                <Link to={'/custom/liveries'} title="Подробнее">
-                    <li class={styles.item}>
-                        <img src={card3} alt="img" class={styles.images_card} />
-                        <h4 class={styles.heading_card}>Ливреи</h4>
-                        <p class={styles.description_card}>Создадим с нуля или воспроизведём по примерам дизайн гоночного автомобиля</p>
-                        <div class={styles.price_card}>2000₽</div>
+                    <li className={styles.item}>
+                        <img src={card3} alt="img" className={styles.images_card} />
+                        <h4 className={styles.heading_card}>Ливреи</h4>
+                        <p className={styles.description_card}>Создадим с нуля или воспроизведём по примерам дизайн гоночного автомобиля</p>
+                        <div className={styles.price_card}>2000₽</div>
                         <OrderButton link='/'/>
                     </li>
                 </Link>
