99fcc7b6598f5ed8c952dd6aaa7826068c19bb4f
SilantievMax <nohrin.max@yandex.ru>
2022-08-28 20:30:34 +0300

    add component StagesOfClasses

--- src/pages/PersonalClassesPage/PersonalClassesPage.jsx
+++ src/pages/PersonalClassesPage/PersonalClassesPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import BannerPersonalClasses from "../../components/ComponentsForPersonalClassesPage/BannerPersonalClasses/BannerPersonalClasses";
 import InfoAboutClasses from "../../components/ComponentsForPersonalClassesPage/InfoAboutClasses/InfoAboutClasses";
+import StagesOfClasses from "../../components/ComponentsForPersonalClassesPage/StagesOfClasses/StagesOfClasses";
 
 const PersonalClassesPage = () => {
     return (
         <>
         <BannerPersonalClasses />
         <InfoAboutClasses />
+        <StagesOfClasses />
         </>
     )
 }
