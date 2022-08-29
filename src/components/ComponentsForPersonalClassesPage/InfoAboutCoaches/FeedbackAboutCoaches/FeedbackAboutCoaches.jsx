d1051f07b2b7bb8cba231c85506ea84f2aedc065
SilantievMax <nohrin.max@yandex.ru>
2022-08-29 09:57:43 +0300

    modified component FeedbackAboutCoaches

--- src/components/ComponentsForPersonalClassesPage/InfoAboutCoaches/FeedbackAboutCoaches/FeedbackAboutCoaches.jsx
+++ src/components/ComponentsForPersonalClassesPage/InfoAboutCoaches/FeedbackAboutCoaches/FeedbackAboutCoaches.jsx
@@ -5,126 +5,126 @@ const FeedbackAboutCoaches = () => {
     return (
         <>
         <ul className={styles.list}>
-            <li className="item">
-                <h5 className="heading">Отзывы о тренере</h5>
-                <div className="container">
-                    <ul className="list">
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+            <li className={styles.item}>
+                <h5 className={styles.heading}>Отзывы о тренере</h5>
+                <div className={styles.container}>
+                    <ul className={styles.list_text}>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li className="item">
-                <h5 className="heading">Отзывы о тренере</h5>
-                <div className="container">
-                    <ul className="list">
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
-                        </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
-                        </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
-                        </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
-                        </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
-                        </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
                     </ul>
                 </div>
             </li>
-            <li className="item">
-                <h5 className="heading">Отзывы о тренере</h5>
-                <div className="container">
-                    <ul className="list">
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+            <li className={styles.item}>
+                <h5 className={styles.heading}>Отзывы о тренере</h5>
+                <div className={styles.container}>
+                    <ul className={styles.list_text}>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
                     </ul>
                 </div>
             </li>
-            <li className="item">
-                <h5 className="heading">Отзывы о тренере</h5>
-                <div className="container">
-                    <ul className="list">
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+            <li className={styles.item}>
+                <h5 className={styles.heading}>Отзывы о тренере</h5>
+                <div className={styles.container}>
+                    <ul className={styles.list_text}>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
-                        <li className="item">
-                            <p className="info">Данила Гришанов <span className="day">12 дней назад</span></p>
-                            <span className="message">Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li className={styles.item}>
+                <h5 className={styles.heading}>Отзывы о тренере</h5>
+                <div className={styles.container}>
+                    <ul className={styles.list_text}>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        </li>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        </li>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        </li>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        </li>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
+                        </li>
+                        <li className={styles.item_text}>
+                            <p className={styles.info}>Данила Гришанов <span className={styles.day}>12 дней назад</span></p>
+                            <span className={styles.message}>Привет, ты очень крутой, смотрю тебя и повторяю за тобой, спасибо большое за то, что</span>
                         </li>
                     </ul>
                 </div>
