1be865ef6a86781baa55484c3401a27035994f33
SilantievMax <nohrin.max@yandex.ru>
2022-10-07 21:04:12 +0300

    Registration implemented

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -12,7 +12,6 @@ const LoginForm = () => {
     const {
         register,
         handleSubmit,
-        setError,
         formState: { errors, isValid },
     } = useForm({
         defaultValues: {
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,56 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './registerForm.css'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { Navigate } from "react-router-dom";
-import { fetchAuth, selectIsAuth } from "../../redux/features/authSlice";
-
+import { fetchAuth, fetchRegister, selectIsAuth } from "../../redux/features/authSlice";
+import "./registerForm.css";
 
 const RegisterForm = () => {
     const isAuth = useSelector(selectIsAuth);
     const dispatch = useDispatch();
     const {
-      register,
-      handleSubmit,
-      setError,
-      formState: { errors, isValid },
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
     } = useForm({
-      defaultValues: {
-        fullName: 'Test testov',
-        email: "test12334@test.com",
-        password: "xxXX1234",
-      },
-      mode: "onChange",
+        defaultValues: {
+            fullName: "Test Testov",
+            email: "test12334@test.com",
+            password: "xxXX1234",
+        },
+        mode: "onChange",
     });
 
     const onSubmit = async (values) => {
-        const data = await dispatch(fetchAuth(values));
+        const data = await dispatch(fetchRegister(values));
         if (!data.payload) {
-          return alert("Не удалось авторизоваться!");
+            return alert("Не удалось зарегистрироваться!");
         }
         if ("token" in data.payload) {
-          window.localStorage.setItem("token", data.payload.token);
+            window.localStorage.setItem("token", data.payload.token);
         }
-      };
-    
-      if (isAuth) {
+    };
+
+    if (isAuth) {
         return <Navigate to="/administrator/statistics" />;
-      }
+    }
 
     return (
         <>
-        <form onSubmit={(e) => e.preventDefault()} action="" onsubmit="return false" className="register__form">
-            <h3 className="register__heading">Регистрация</h3>
-            <input type="text" autocomplete="off" className="register__input" placeholder='Имя и Фамилия*' />
-            <input type="text" autocomplete="off" className="register__input" placeholder='E-mail*' />
-            <input type="password" autocomplete="off" className="register__input" placeholder='Пароль' />
-            {/* <input type="password" autocomplete="off" className="register__input" placeholder='Повторите пароль'/> */}
-            <button type='submit' className="register__btn">Зарегистрироваться</button>
-            <Link to="/authorization/login" className="register__login">Войти</Link>
-        </form>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                action=""
+                onsubmit="return false"
+                className="register__form"
+            >
+                <h3 className="register__heading">Регистрация</h3>
+                <input
+                    type="text"
+                    autocomplete="off"
+                    className="register__input"
+                    placeholder="Имя и Фамилия*"
+                    {...register("fullName", { required: "Укажите полное имя" })}
+                />
+                <input
+                    type="text"
+                    autocomplete="off"
+                    className="register__input"
+                    placeholder="E-mail*"
+                    {...register("email", { required: "Укажите email" })}
+                />
+                <input
+                    type="password"
+                    autocomplete="off"
+                    className="register__input"
+                    placeholder="Пароль"
+                    {...register("password", { required: "Укажите пароль" })}
+                />
+                {/* <input type="password" autocomplete="off" className="register__input" placeholder='Повторите пароль'/> */}
+                <button type="submit" className="register__btn">
+                    Зарегистрироваться
+                </button>
+                <Link to="/authorization/login" className="register__login">
+                    Войти
+                </Link>
+            </form>
         </>
-    )
-}
+    );
+};
 
 export default RegisterForm;
