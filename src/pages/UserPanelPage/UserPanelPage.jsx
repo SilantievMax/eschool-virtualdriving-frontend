3f555eff681ce660595592fcc83ddae59963af75
SilantievMax <nohrin.max@yandex.ru>
2022-11-14 20:52:29 +0300

    rendering user orders

--- src/pages/UserPanelPage/UserPanelPage.jsx
+++ src/pages/UserPanelPage/UserPanelPage.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AdminAside from "../../components/AdminPanelPageComponents/AdminAside/AdminAside";
-import { fetcOrdersSetupUers, fetcOrdersTrainingUers } from "../../redux/features/ordersUserSlice";
+import {
+    fetcOrdersSetupUers,
+    fetcOrdersTrainingUers,
+} from "../../redux/features/ordersUserSlice";
 
 const UserPanelPage = () => {
     const dispatch = useDispatch();
@@ -11,13 +14,42 @@ const UserPanelPage = () => {
         dispatch(fetcOrdersSetupUers());
     }, []);
 
-    const { orders } = useSelector((state) => state.ordersUser);
+    const { items1, items2 } = useSelector((state) => state.ordersUser.orders);
 
-    console.log(orders);
+    console.log(items1);
+    console.log(items2);
     return (
         <>
             <AdminAside />
-            <div className="ml-56 bg-slate-50 py-3"></div>
+            <div className="ml-56 bg-slate-50 py-3 flex">
+                <ul>
+                    {items1 &&
+                        items1.map((item) => (
+                            <li className="border" key={item._id}>
+                                <p>{item.orderNumber}</p>
+                                <p>{item.orderName}</p>
+                                <p>{item.orderDate}</p>
+                                <p>{item.status}</p>
+                            </li>
+                        ))}
+                </ul>
+                <ul>
+                    {items2 &&
+                        items2.map((item) => (
+                            <li className="border" key={item.file._id}>
+                                <p>{item.file.orderNumber}</p>
+                                <p>{item.file.orderName}</p>
+                                <p>{item.file.orderDate}</p>
+                                <p>{item.file.status}</p>
+                                <a href={item.accessLink}>
+                                    {item.accessLink
+                                        ? item.accessLink
+                                        : "нет ссылки"}
+                                </a>
+                            </li>
+                        ))}
+                </ul>
+            </div>
         </>
     );
 };
