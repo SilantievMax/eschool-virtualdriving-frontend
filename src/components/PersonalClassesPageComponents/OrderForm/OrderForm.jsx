7c8c23b724e33992491b222ef66c90323a99aa0d
SilantievMax <nohrin.max@yandex.ru>
2023-01-25 15:17:17 +0300

    modified order form

--- src/components/PersonalClassesPageComponents/OrderForm/OrderForm.jsx
+++ src/components/PersonalClassesPageComponents/OrderForm/OrderForm.jsx
@@ -2,131 +2,117 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { fetcCars, fetcTracks } from "../../../redux/features/dataSlice.js";
+import img1 from "../../../assets/images/order1.webp";
 import axios from "../../../utils/axios.js";
 import styles from "./OrderForm.module.css";
 
 const OrderForm = () => {
-    const [isLoading, setLoading] = React.useState(false);
-    const [communications, setCommunications] = React.useState("89257777777");
-    const [orderDate, setOrderDate] = React.useState();
-    const [car, setCar] = React.useState("BMW M4GT3");
-    const [track, setTrack] = React.useState("Monza");
-    const [experience, setExperience] = React.useState(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum convallis maximus neque, sit amet lacinia ipsum mattis ac. Integer mollis."
-    );
-    const [coment, setComent] = React.useState(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at elit tortor. Pellentesque ultricies metus maximus massa porttitor auctor. Nulla."
-    );
-    const [equipment, setEquipment] = React.useState(
-        "Thrustmaster T3PM Pedals, Thrustmaster T-LCM Rubber Grip, Speedlink DRIFT O.Z."
-    );
-    const [price, setPrice] = React.useState(1500);
+  const [isLoading, setLoading] = React.useState(false);
+  const [communications, setCommunications] = React.useState("89257777777");
+  const [orderDate, setOrderDate] = React.useState();
+  const [car, setCar] = React.useState("BMW M4GT3");
+  const [track, setTrack] = React.useState("Monza");
+  const [experience, setExperience] = React.useState(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum convallis maximus neque, sit amet lacinia ipsum mattis ac. Integer mollis."
+  );
+  const [coment, setComent] = React.useState(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at elit tortor. Pellentesque ultricies metus maximus massa porttitor auctor. Nulla."
+  );
+  const [equipment, setEquipment] = React.useState("Thrustmaster T3PM Pedals, Thrustmaster T-LCM Rubber Grip, Speedlink DRIFT O.Z.");
+  const [price, setPrice] = React.useState(1500);
 
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetcCars());
-        dispatch(fetcTracks());
-    }, []);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetcCars());
+    dispatch(fetcTracks());
+  }, []);
 
-    const { cars } = useSelector((state) => state.data);
-    const { tracks } = useSelector((state) => state.data);
+  const { cars } = useSelector((state) => state.data);
+  const { tracks } = useSelector((state) => state.data);
 
-    console.log(cars);
+  console.log(cars);
 
-    const onSubmit = async () => {
-        try {
-            setLoading(true);
+  const onSubmit = async () => {
+    try {
+      setLoading(true);
 
-            const fields = {
-                car,
-                track,
-                orderDate,
-                communications,
-                experience,
-                coment,
-                equipment,
-                price,
-            };
+      const fields = {
+        car,
+        track,
+        orderDate,
+        communications,
+        experience,
+        coment,
+        equipment,
+        price,
+      };
 
-            const { data } = await axios.post("/orders/training", fields);
+      const { data } = await axios.post("/orders/training", fields);
 
-            const id = data._id;
-            if (data) {
-                toast.success("Заказ создан");
-            }
-            setCommunications("");
-        } catch (err) {
-            console.warn(err.response.data);
-            toast.error("Ошибка при создании заказа!");
-        }
-    };
+      const id = data._id;
+      if (data) {
+        toast.success("Заказ создан");
+      }
+      setCommunications("");
+    } catch (err) {
+      console.warn(err.response.data);
+      toast.error("Ошибка при создании заказа!");
+    }
+  };
 
-    return (
-        <div>
-            <div className={styles.wrapper}>
-                <h3>Выбирите автомобиль:</h3>
-                <select
-                    className={styles.input}
-                    value={car}
-                    onChange={(e) => setCar(e.target.value)}
-                    name="pets"
-                >
-                    {cars.map(({ title }) => (
-                        <option value={title}>{title}</option>
-                    ))}
-                </select>
-                <h3>Выбирите трассу:</h3>
-                <select
-                    className={styles.input}
-                    value={track}
-                    onChange={(e) => setTrack(e.target.value)}
-                    name="pets"
-                >
-                    {tracks.map(({title}) => (
-                        <option value={title}>{title}</option>
-                    ))}
-                </select>
-                <h3>Выбирите дату тренировки:</h3>
-                <input
-                    className={styles.input}
-                    type="datetime-local"
-                    value={orderDate}
-                    onChange={(e) => setOrderDate(e.target.value)}
-                    placeholder="orderDate"
-                />
-                <h3>Комментарий:</h3>
-                <textarea
-                    className={styles.input}
-                    value={coment}
-                    onChange={(e) => setComent(e.target.value)}
-                    placeholder="coment"
-                />
-                <h3>Как с вами связаться:</h3>
-                <input
-                    className={styles.input}
-                    type="text"
-                    value={communications}
-                    onChange={(e) => setCommunications(e.target.value)}
-                    placeholder="communications"
-                />
+  return (
+    <>
+      <ul className={styles.order__list}>
+        <li className={styles.orser__item}>
+          <img className={styles.img} src={img1} alt="img1" />
+        </li>
+        <li className={styles.orser__item}>
+          <label className={styles.label}>
+            Выберите автомобиль:
+            <select className={styles.input} value={car} onChange={(e) => setCar(e.target.value)} name="pets">
+              {cars.map(({ title }) => (
+                <option value={title}>{title}</option>
+              ))}
+            </select>
+          </label>
+          <label className={styles.label}>
+            Выберите трассу:
+            <select className={styles.input} value={track} onChange={(e) => setTrack(e.target.value)} name="pets">
+              {tracks.map(({ title }) => (
+                <option value={title}>{title}</option>
+              ))}
+            </select>
+          </label>
+          <label className={styles.label}>
+            Выберите дату тренировки:
+            <input className={styles.input} type="datetime-local" value={orderDate} onChange={(e) => setOrderDate(e.target.value)} placeholder="orderDate" />
+          </label>
+          <label className={styles.label}>
+            Комментарий:
+            <textarea type="textarea" className={styles.input} value={coment} onChange={(e) => setComent(e.target.value)} placeholder="coment" />
+          </label>
+          <label className={styles.label}>
+            Как с вами связаться:
+            <input
+              className={styles.input}
+              type="text"
+              value={communications}
+              onChange={(e) => setCommunications(e.target.value)}
+              placeholder="communications"
+            />
+          </label>
+        </li>
+        <li className={styles.orser__item}>
+          <label className={styles.label}>
+            Расскажите о вашем опыте:
+            <textarea type="textarea" className={styles.input} value={experience} onChange={(e) => setExperience(e.target.value)} placeholder="experience" />
+          </label>
 
-                <h3>Расскажите о вашем опыте:</h3>
-                <textarea
-                    className={styles.input}
-                    type="text"
-                    value={experience}
-                    onChange={(e) => setExperience(e.target.value)}
-                    placeholder="experience"
-                />
-                <h3>Какое оборудование вы используете?:</h3>
-                <textarea
-                    className={styles.input}
-                    type="text"
-                    value={equipment}
-                    onChange={(e) => setEquipment(e.target.value)}
-                    placeholder="equipment"
-                />
-                {/* <h3>Цена:</h3>
+          <label className={styles.label}>
+            Какое оборудование вы используете?:
+            <textarea type="textarea" className={styles.input} value={equipment} onChange={(e) => setEquipment(e.target.value)} placeholder="equipment" />
+          </label>
+          {/* <label className={styles.label}>Цена:
                 <input
                     className={styles.input}
                     type="number"
@@ -135,12 +121,20 @@ const OrderForm = () => {
                     placeholder="price"
                 /> */}
 
-                <button className={styles.btn} onClick={onSubmit}>
-                    Отправить
-                </button>
-            </div>
-        </div>
-    );
+          <label className={styles.checkbox}>
+            <input className={styles.checkbox__input} type="checkbox" />
+            <span>Я принимаю условия передачи информации</span>
+          </label>
+
+          <small className={styles.warning}>Поля отмеченные* являются обязательными</small>
+
+          <button className={styles.btn} onClick={onSubmit}>
+            Отправить
+          </button>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default OrderForm;
--- src/pages/PersonalClassesPage/PersonalClassesPage.jsx
+++ src/pages/PersonalClassesPage/PersonalClassesPage.jsx
@@ -14,8 +14,8 @@ const PersonalClassesPage = () => {
       {/* <BannerPersonalClasses /> */}
       <Layout>
         <Text />
+        <OrderForm />
       </Layout>
-      <OrderForm />
       <Footer />
     </>
   );
