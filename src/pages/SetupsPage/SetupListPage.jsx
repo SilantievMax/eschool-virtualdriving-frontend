7921c22232d3cf7eda69001bcb6e361613902f2c
Kirill Gladkikh <onest.kill@yandex.ru>
2023-01-31 03:28:50 +0400

    setup list page layout

--- src/pages/SetupsPage/SetupListPage.jsx
+++ src/pages/SetupsPage/SetupListPage.jsx
@@ -1,6 +1,14 @@
-import React from "react";
 import { SetupList } from "../../components/SetupsPageComponents/components/SetupList";
+import Layout from "../../components/GeneralComponents/Layout/Layout";
+// import Footer from "../../components/GeneralComponents/Footer/Footer";
 
 export const SetupListPage = () => {
-  return <SetupList />;
+  return (
+    <>
+      <Layout>
+        <SetupList />
+      </Layout>
+      {/* <Footer /> */}
+    </>
+  );
 };
