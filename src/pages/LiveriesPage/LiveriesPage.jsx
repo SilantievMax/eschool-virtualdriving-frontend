177791f9e6c0ef9e3f312b8cb003a70623820fc9
SilantievMax <nohrin.max@yandex.ru>
2023-01-31 12:01:46 +0300

    modified Banner components

--- src/pages/LiveriesPage/LiveriesPage.jsx
+++ src/pages/LiveriesPage/LiveriesPage.jsx
@@ -3,6 +3,7 @@ import Layout from "../../components/GeneralComponents/Layout/Layout";
 import Banner from "../../components/LiveriesPageComponents/Banner/Banner";
 import Heading from "../../components/LiveriesPageComponents/Heading/Heading";
 import OrderForm from "../../components/LiveriesPageComponents/OrderForm/OrderForm"
+import Footer from "../../components/GeneralComponents/Footer/Footer"
 import banner1 from "../../assets/images/banner (1).webp"
 import banner2 from "../../assets/images/banner (2).webp"
 import banner3 from "../../assets/images/banner (3).webp"
@@ -18,7 +19,12 @@ const LiveriesPage = () => {
         <Banner banner={banner3}/>
         <OrderForm />
         <Banner banner={banner2}/>
+        <Banner banner={banner1}/>
+        <Banner banner={banner4}/>
+        <Banner banner={banner5}/>
+        <Banner banner={banner6}/>
       </Layout>
+      <Footer />
     </>
   );
 };
