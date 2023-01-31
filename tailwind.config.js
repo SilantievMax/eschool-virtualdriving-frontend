2032cea4e97521157695f076ecdd6bbfe1a58264
Kirill Gladkikh <onest.kill@yandex.ru>
2023-01-31 04:04:41 +0400

    fix global fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Gilroy', 'sans-serif'],
+      header: ['Bebas Neue Cyrillic', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
