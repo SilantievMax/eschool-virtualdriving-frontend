cbb4ab0b6a02889ae73d6eae953f76d97976af21
Kirill Gladkikh <onest.kill@yandex.ru>
2023-01-31 15:26:53 +0400

    setup element page layout

--- src/pages/SetupsPage/SetupElementPage.jsx
+++ src/pages/SetupsPage/SetupElementPage.jsx
@@ -1,8 +1,17 @@
 import { useParams } from "react-router-dom";
 import { SetupElement } from "../../components/SetupsPageComponents/components/SetupElement";
+import Layout from "../../components/GeneralComponents/Layout/Layout";
+// import Footer from "../../components/GeneralComponents/Footer/Footer";
 
 export const SetupElementPage = () => {
   let params = useParams();
 
-  return <SetupElement id={params.id} />;
+  return (
+    <>
+      <Layout>
+        <SetupElement id={params.id} />
+      </Layout>
+      {/* <Footer /> */}
+    </>
+  );
 };
